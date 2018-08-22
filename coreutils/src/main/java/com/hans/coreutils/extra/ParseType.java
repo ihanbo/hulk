9c41a7c43adc708368abdcf1d5b63a53eddcf780
hanbo <hanbo@didichuxing.com>
2018-08-22 16:39:29 +0800

    no message

--- coreutils/src/main/java/com/hans/coreutils/extra/ParseType.java
+++ coreutils/src/main/java/com/hans/coreutils/extra/ParseType.java
@@ -1,6 +1,7 @@
 package com.hans.coreutils.extra;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
@@ -17,17 +18,23 @@ import java.util.Arrays;
  */
 public class ParseType<T> {
 
-
+    final Class<? super T> rawType;
     public Type mType;
 
     public ParseType() {
         mType = getSuperclassTypeParameter(getClass());
+        this.rawType = (Class<? super T>) getRawType(mType);
     }
 
 
-    public Type getType() {
+    public final Type getType() {
         return mType;
     }
+
+    public final Class<? super T> getRawType() {
+        return rawType;
+    }
+
 
     static Type getSuperclassTypeParameter(Class<?> subclass) {
         Type superclass = subclass.getGenericSuperclass();
@@ -60,6 +67,40 @@ public class ParseType<T> {
         } else {
             // type is either serializable as-is or unsupported
             return type;
+        }
+    }
+
+    static Class<?> getRawType(Type type) {
+        if (type instanceof Class<?>) {
+            // type is a normal class.
+            return (Class<?>) type;
+
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+
+            // I'm not exactly sure why getRawType() returns Type instead of Class.
+            // Neal isn't either but suspects some pathological case related
+            // to nested classes exists.
+            Type rawType = parameterizedType.getRawType();
+            checkArgument(rawType instanceof Class);
+            return (Class<?>) rawType;
+
+        } else if (type instanceof GenericArrayType) {      //User[] 类型
+            Type componentType = ((GenericArrayType) type).getGenericComponentType();
+            return Array.newInstance(getRawType(componentType), 0).getClass();
+
+        } else if (type instanceof TypeVariable) {  //T
+            // we could use the variable's bounds, but that won't work if there are multiple.
+            // having a raw type that's more general than necessary is okay
+            return Object.class;
+
+        } else if (type instanceof WildcardType) {  //? extends String 返回String
+            return getRawType(((WildcardType) type).getUpperBounds()[0]);
+
+        } else {
+            String className = type == null ? "null" : type.getClass().getName();
+            throw new IllegalArgumentException("Expected a Class, ParameterizedType, or "
+                    + "GenericArrayType, but <" + type + "> is of type " + className);
         }
     }
 
