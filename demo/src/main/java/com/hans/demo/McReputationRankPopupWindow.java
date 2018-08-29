74c0fa62de1bf4745ae6fd9f26b213208375b614
hanbo <hanbo@didichuxing.com>
2018-08-29 19:59:26 +0800

    no message

---
+++ demo/src/main/java/com/hans/demo/McReputationRankPopupWindow.java
@@ -0,0 +1,39 @@
+package com.hans.demo;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+
+/**
+ * @Author hanbo
+ * @Since 2018/8/29
+ */
+public class McReputationRankPopupWindow extends PopupWindow {
+
+    private View mView;
+    public McReputationRankPopupWindow(Context context) {
+        super(context);
+
+        //mView = LayoutInflater.from(context).inflate(R.layout.mc_reputation_rank_list_popupwindow, null);
+
+
+
+        //设置PopupWindow的View
+        this.setContentView(mView);
+        //设置PopupWindow弹出窗体的宽
+        this.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+        //设置PopupWindow弹出窗体的高
+        this.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        //设置PopupWindow弹出窗体可点击
+        this.setFocusable(true);
+        //设置SelectPicPopupWindow弹出窗体动画效果
+        //this.setAnimationStyle(R.style.mc_reputation_rank_list_select_anim_style);
+        //实例化一个ColorDrawable颜色为半透明
+        ColorDrawable dw = new ColorDrawable(0xb0000000);
+        //设置SelectPicPopupWindow弹出窗体的背景
+        this.setBackgroundDrawable(dw);
+    }
+}
